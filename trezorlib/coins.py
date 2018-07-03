aab41780b065688fc606a354c38cf39bb9c17b10
D.Matskevych <dimqqqq@gmail.com>
2018-07-03 12:56:50 +0300

    fix coins missing

--- trezorlib/coins.py
+++ trezorlib/coins.py
@@ -35,11 +35,14 @@ def _load_coins_json():
 
 
 def _insight_for_coin(coin):
-    if not coin['bitcore']:
+    if not coin['bitcore'] and not coin['blockbook']:
         return None
     zcash = coin['coin_name'].lower().startswith('zcash')
     network = 'insight_{}'.format(coin['coin_name'].lower().replace(' ', '_'))
-    url = coin['bitcore'][0] + '/api/'
+    if not coin['bitcore']:
+        url = coin['blockbook'][0] + '/api/'
+    else:
+        url = coin['bitcore'][0] + '/api/'
     return TxApiInsight(network=network, url=url, zcash=zcash)
 
 
@@ -54,7 +57,7 @@ except Exception as e:
 slip44 = {name: coin['slip44'] for name, coin in by_name.items()}
 tx_api = {name: _insight_for_coin(coin)
           for name, coin in by_name.items()
-          if coin["bitcore"]}
+          if coin["bitcore"] or coin['blockbook']}
 
 # fixup for Dogecoin
 tx_api['Dogecoin'] = TxApiBlockCypher(network='blockcypher_dogecoin', url='https://api.blockcypher.com/v1/doge/main/')
